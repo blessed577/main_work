37f2698c8eabee9197667c33411c42e938651611
blessed577 <124183668+blessed577@users.noreply.github.com>
2023-10-25 15:08:43 -0700

    sec file

---
+++ conversion_un.py
@@ -0,0 +1,19 @@
+x = 38  # int
+y = 8.4 # float
+z = 1+6j # cmplex
+
+#convert from int to float:
+a=float(38)
+#convert from float to int:
+b=int(8.4)
+#convert from int to complex:
+c=complex(38)
+
+print(a)
+print(b)
+print(c)
+
+
+print(type(a))
+print(type(b))
+print(type(c))
